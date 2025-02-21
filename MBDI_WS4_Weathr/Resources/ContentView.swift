5c7f2944c6f8ddfa1ef3c27d18595cb975485930
Emre Sagir <e.sagir@student.avans.nl>
2025-02-21 12:25:41 +0100

    update

--- MBDI_WS4_Weathr/Resources/ContentView.swift
+++ MBDI_WS4_Weathr/Resources/ContentView.swift
@@ -32,12 +32,31 @@ struct ContentView: View {
         var temperature = "?"
         guard let url = URL(string:urlString) else {
             print("ERROR: failed to construct a URL from string"); return}
-        print(url)
         let task = URLSession.shared.dataTask(with: url) {
             data, response, error in
+            
+            if let error = error{
+                print("ERROR: fetch failed: \(error.localizedDescription)")
+                return
+            }
+            guard let data = data else{
+                print("ERROR: no data returned from API call")
+                return
+            }
+            var newWeatherData: WeatherData?
+            do{
+                newWeatherData = try JSONDecoder().decode(WeatherData.self, from: data)
+            } catch let error as NSError{
+                print("ERROR: decodig. In domain = \(error.domain), description = \(error.localizedDescription)")
+            }
+            if newWeatherData == nil{
+                print("ERROR: read or decoding failed")
+                return
+            }
+            self.weatherData = newWeatherData
+            print("succes")
         }
-        print(task.response)
-        currentTemperature = temperature
+        task.resume()
         
     }
     
